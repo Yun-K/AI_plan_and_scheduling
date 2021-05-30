43e3253636abfec3dca0b296024b3da274df9263
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-05-31 06:37:09 +0800

    implement the NN

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import numpy as np
 
 
 def main():
-    # temp =[]`
-    # for i in temp:
-    #     print(i)
-
-    # if 1==1:
-    #     print("finished:")
-    #     return`
-
     # Paths to the data and solution files.
     vrp_file = "data/n32-k5.vrp"  # "data/n80-k10.vrp"
     sol_file = "data/n32-k5.sol"  # "data/n80-k10.sol"
@@ -32,10 +24,12 @@ def main():
     # Executing and visualizing the nearest neighbour VRP heuristic.
     # Uncomment it to do your assignment!
 
-    # nnh_solution = nearest_neighbour_heuristic(px, py, demand, capacity, depot)
-    # nnh_distance = utility.calculate_total_distance(nnh_solution, px, py, depot)
-    # print("Nearest Neighbour VRP Heuristic Distance:", nnh_distance)
-    # utility.visualise_solution(nnh_solution, px, py, depot, "Nearest Neighbour Heuristic")
+    nnh_solution = nearest_neighbour_heuristic(px, py, demand, capacity, depot)
+    nnh_distance = utility.calculate_total_distance(
+        nnh_solution, px, py, depot)
+    print("Nearest Neighbour VRP Heuristic Distance:", nnh_distance)
+    utility.visualise_solution(
+        nnh_solution, px, py, depot, "Nearest Neighbour Heuristic")
 
     # Executing and visualizing the saving VRP heuristic.
     # Uncomment it to do your assignment!
@@ -59,46 +53,57 @@ def nearest_neighbour_heuristic(px, py, demand, capacity, depot):
     """
 
     # TODO - Implement the Nearest Neighbour Heuristic to generate VRP solutions.
+    routesToReturn = []  # list of routes to return
+    # startNode = depot  # start at the depot node
 
     # step 1: Initialisea solution: a route starting from the depot
-    routesToReturn = []  # list of routes to return
-    startNode = depot  # start at the depot node
-    currentDemand = 0
-    ids = np.arrange(len(px))
-    ids.remove(0)  # remove the depot node
+    # initial set up the list with out the depot node
+    node_index_list = []
+    for time in range(len(px)):
+        if(time == 0):  # discount the depot node
+            continue
+        node_index_list.append(time)
 
-    totalDemandRoute = 0
     # loop until all nodes are visited
-    while len(ids) > 0:
+    while len(node_index_list) > 0:
+        startNode = depot  # start at the depot node
         visited_invalid_Nodes = []
-        # 
-        for time in range(len(ids)):
-            nearest_distance = 999999999999999999999999
-            index = -1  # index of the nearest node of current start node
+        temp_route = []
+        # loop through all nodes to find the route until no feasible node is found for the current route
+        for time in range(len(node_index_list)):
+            node_id_index = -1  # index of the nearest node of current start node
+            nearest_distance = 9999999999999999
             # loop through all unvisited nodes to find the feasible node
-            for i in ids:
+            for node in node_index_list:
                 # check if it is the feasible node
-                if i == startNode or i == depot or i in visited_invalid_Nodes:
+                if node == startNode or node == depot or node in visited_invalid_Nodes:
                     continue
                 # check if current node is nearest
                 if nearest_distance > utility.calculate_euclidean_distance(
-                        px, py, startNode, i):
-                    index = i
+                        px, py, startNode, node):
+                    node_id_index = node
                     nearest_distance = utility.calculate_euclidean_distance(
-                        px, py, startNode, i)
+                        px, py, startNode, node)
 
-            if index == -1:
-                break  # break it since every node has visited
+            if node_id_index == -1:
+                break  # close the current route since every node are not feasible
+
+            # calculate the total demand of all nodes that are in the route
+            total_route_demand = demand[node_id_index]
+            for existNode in temp_route:
+                total_route_demand += demand[existNode]
 
             # check whether the total demand of the route doesn't exceed the capacity
-            totalDemandRoute += demand[index]
-            if totalDemandRoute > capacity:
-                visited_invalid_Nodes.append(index)
+            if total_route_demand > capacity:
+                visited_invalid_Nodes.append(node_id_index)
+            # not exceed, append the node to the route,
+            else:
+                temp_route.append(node_id_index)
+                startNode = node_id_index
+                node_index_list.remove(node_id_index)
 
-        # After inserting the node, the total demand of the route does not exceed the capacity
-        # 3.If no feasible node is found for the current route, then close the current route (return to the depot)and create a new route starting from the depot4.Repeat 2. and 3. until all nodes are visited
-
-    # while
+        # finish current route, so append the current route to the route to return list
+        routesToReturn.append(temp_route)
 
     return routesToReturn
 
